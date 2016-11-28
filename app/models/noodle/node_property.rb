75dd0b2a81ad2f23b4ed54de3ccfbcc5bb72a082
LA <leszek.albrzykowski@nobleprog.pl>
2016-11-29 00:48:31 +0100

    Refactor of tests

--- app/models/noodle/node_property.rb
+++ app/models/noodle/node_property.rb
@@ -1,4 +1,5 @@
-require_relative 'concerns/value_validable'
+require 'date'
+
 require_relative 'validators/empty_validator'
 require_relative 'validators/blank_validator'
 require_relative 'validators/min_length_validator'
@@ -21,11 +22,44 @@ module Noodle
                    Noodle::MaxValidator
     
     def value=(value)
-      self["#{self.node_class_property.properties['type']}_value"] = value
+      self["#{self.node_class_property.properties['type']}_value"] = cast value
     end
 
     def value
       self["#{self.node_class_property.properties['type']}_value"]
+    end
+    
+    private
+    def cast(value)
+      case self.node_class_property.properties['type']
+      when 'binary'
+        return value.b
+      when 'boolean'
+        return !!value
+      when 'date'
+        return value.to_date
+      when 'datetime'
+        return value.to_datetime
+      when 'decimal'
+        return value.to_f
+      when 'float'
+        return value.to_f
+      when 'integer'
+        return value.to_i
+      when 'bigint'
+        return value.to_i
+      when 'string'
+        return value.to_s
+      when 'text'
+        return value.to_s
+      when 'time'
+        return value.to_time
+      when 'timestamp'
+        # @todo try to find fix with nil in tests
+        return value
+      else
+        raise 'Unrecognized value type'
+      end
     end
   end
 end
---
+++ spec/models/node_property_validation_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+require 'date'
+
+RSpec.describe Noodle::NodeProperty, :type => :model do
+  context "properly validates value before save" do
+    before(:each) do
+      @node_class = Noodle::NodeClass.new(name: 'node_class')
+      @node = Noodle::Node.new(node_class: @node_class)
+      @node_class_property = Noodle::NodeClassProperty.new(
+        name: 'node_class_property',
+        node_class: @node_class
+      )
+      @node_property = Noodle::NodeProperty.new(node: @node)
+      @node_property.node_class_property = @node_class_property
+    end
+    it "empty validator raise error if value is empty" do
+      @node_class_property.properties = {type: 'string', validators: {empty: false}}
+      @node_property.value = ''
+      expect{@node_property.save}.to raise_error
+      @node_property.value = 'str'
+      expect{@node_property.save}.not_to raise_error
+    end
+    it "blank validator raise error if value is blank" do
+      @node_class_property.properties = {type: 'string', validators: {blank: false}}
+      @node_property.value = ' '
+      expect{@node_property.save}.to raise_error
+      @node_property.value = 'str'
+      expect{@node_property.save}.not_to raise_error
+    end
+    it "min length validator raise error if string value is to short" do
+      @node_class_property.properties = {type: 'string', validators: {min_length: 5}}
+      @node_property.value = 'str'
+      expect{@node_property.save}.to raise_error
+      @node_property.value = 'strstr'
+      expect{@node_property.save}.not_to raise_error
+    end
+    it "max length validator raise error if string value is to long" do
+      @node_class_property.properties = {type: 'string', validators: {max_length: 1}}
+      @node_property.value = 'str'
+      expect{@node_property.save}.to raise_error
+      @node_property.value = 's'
+      expect{@node_property.save}.not_to raise_error
+    end
+    it "min length validator raise error if value is lesser than *" do
+      @node_class_property.properties = {type: 'string', validators: {min: 10}}
+      @node_property.value = 9
+      expect{@node_property.save}.to raise_error
+      @node_property.value = 11
+      expect{@node_property.save}.not_to raise_error
+    end
+    it "max length validator raise error if value is greater than *" do
+      @node_class_property.properties = {type: 'string', validators: {max: 10}}
+      @node_property.value = 11
+      expect{@node_property.save}.to raise_error
+      @node_property.value = 9
+      expect{@node_property.save}.not_to raise_error
+    end
+  end
+end
